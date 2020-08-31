f56793b049a2cd619f962708c3d2b3d227dc9eb4
Jonathan Tay <jonathantay193@gmail.com>
2020-08-31 22:26:33 +0100

    removed unimpared and added ranger

--- bindings.el
+++ bindings.el
@@ -1,9 +1,8 @@
 ;;; ~/.doom.d/bindings.el -*- lexical-binding: t; -*-
 
-
-(map! ;; ported from vim-unimpaired
-      :n  "] p" #'evil-unimpaired-paste-below
-      :n  "[ P" #'evil-unimpaired-paste-above)
-
 (map!
  :n  "SPC o k" #'counsel-yank-pop)
+
+(map! :leader
+      :desc "Open ranger" "f d" #'ranger
+      :desc "Search buffer" "s b" #'counsel-grep)
